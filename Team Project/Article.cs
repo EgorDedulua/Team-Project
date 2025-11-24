f756830e39b2288b3442fd2c17048d87c9576314
Василенка Алексей <alexeyvasilenka.com>
2025-11-24 10:19:03 +0300

    add article

---
+++ Team Project/Article.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Article
+{
+	public Person Author { get; set; }
+	public string Name { get; set; }
+	public double Rating { get; set; }
+	public Article(Person z,string y,double x)
+	{
+		Author = z;
+		Name = y;
+		Rating = x;
+	}
+	public Article() {
+		Author = new Person()
+	}	
+}
